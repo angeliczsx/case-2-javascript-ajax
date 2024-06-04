4f414eef9d37b10e6d549aec43b7036e46d73674
Alvia Dini Nur Lathifah <90056623+angeliczsx@users.noreply.github.com>
2024-06-04 20:10:16 +0700

    Delete Script.js

--- Script.js
+++
@@ -1,8 +0,0 @@
-const toggle = document.getElementById('toggle');
-const chatbox = document.getElementById('chatbox');
-const chatContent = document.getElementById('chat-content');
-const chatForm = document.querySelector('form');
-
-toggle.addEventListener('click', () => {
-    chatbox.classList.toggle('hidden');
-});
